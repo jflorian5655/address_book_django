cb2c326cb99de261149fc495f4d0721aa8c0ae2b
Jeisson <jeisson.florianaco@gmail.com>
2021-07-11 13:00:12 -0500

    configuración proyecto

---
+++ agenda/applications/person/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from model_utils.models import TimeStampedModel
+
+
+class Person(TimeStampedModel):
+    """  Modelo para registrar personas de una agenda  """
+
+    full_name = models.CharField('Nombres', max_length=50)
+    job = models.CharField('Trabajo', max_length=30, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField('teléfono', max_length=15, blank=True)
+
+    class Meta:
+        verbose_name = 'Persona'
+        verbose_name_plural = 'Personas'
+
+    def __str__(self):
+        return self.full_name
